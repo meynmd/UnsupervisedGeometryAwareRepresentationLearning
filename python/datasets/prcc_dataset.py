ac66a8aad4f0cb94a2b9e7063644f222d74ce273
meynm <meynmd@users.noreply.gibhub.com>
2021-01-03 17:01:07 -0800

    simple dataset class

--- python/datasets/prcc_dataset.py
+++ python/datasets/prcc_dataset.py
@@ -1,45 +1,59 @@
 import os
+import random
 from glob import glob
 
+import numpy as np
+import imageio
 import torch
 from torch.utils.data import Dataset
+import torchvision
 
-CAMERAS = ['A', 'B', 'C']
 
-SPLITS = {
-    'train': ['257', '323', '057', '074', '008', '279', '117', '162', '069',
-              '064', '091', '328', '214', '322', '005', '216', '075', '072',
-              '188', '264', '096', '097', '118', '242', '263', '272', '212',
-              '230', '159', '006', '059', '223', '099', '146', '030', '156',
-              '282', '120', '219', '004', '071', '060', '058', '319', '260',
-              '309', '326', '073', '001', '321', '007'],
-    'val': ['094', '152', '112', '070', '061', '062', '028', '018', '324', '265'],
-    'test': ['325', '320', '183', '063', '056', '167', '002', '202', '182', '186']
-}
+CAMERAS = {'A', 'B', 'C'}
+SPLITS = {'train', 'val', 'test'}
+
+
+class Image256toTensor(object):
+    def __call__(self, pic):
+        img = torch.from_numpy(pic.transpose((2, 0, 1))).float()
+        img = img.div(255)
+        return img
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
 
 class PRCCDataset(Dataset):
-    def __init__(self, prcc_root, split):
-        assert split in SPLITS.keys()
+    def __init__(self, prcc_root, split, mean=(0.485, 0.456, 0.406),
+                 stdDev= (0.229, 0.224, 0.225)):
+
+        assert split in SPLITS
         self.split = split
-        self.data_dir = os.path.join(prcc_root, 'rgb', 'test')
+        self.data_dir = os.path.join(prcc_root, 'rgb', split)
+        self.img_dirs = glob(os.path.join(self.data_dir, '*'))
+        self.subj_ids = map(os.path.basename, self.img_dirs)
+        self.data = self.enumerate_data()
+        self.n_images = len(self.data)
+        self.mean = mean
+        self.std_dev = stdDev
+        self.transform_in = torchvision.transforms.Compose([
+            Image256toTensor(),
+            torchvision.transforms.Normalize(self.mean, self.std_dev)
+        ])
 
     def enumerate_data(self):
         data = []
-        for subj_id in SPLITS[self.split]:
-            for camera in CAMERAS:
-                imgs = glob(os.path.join(self.data_dir, camera, subj_id, '*.jpg'))
-                data += [(img, subj_id)]
-
+        for subj_id in self.subj_ids:
+            images = glob(os.path.join(self.data_dir, subj_id, '*.jpg'))
+            data += [(img, subj_id) for img in images]
+        random.shuffle(data)
+        return data
 
     def __len__(self):
-        return self.n_subj*(self.n_subj - 1)
+        return self.n_images
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-
-
-
-
-
+        img_path, subj_id = self.data[idx]
+        img = np.array(self.transform_in(imageio.imread(img_path)),
+                       dtype='float32')
+        return img, subj_id
